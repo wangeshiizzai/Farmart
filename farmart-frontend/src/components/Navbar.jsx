35655f7a213b3e61ba5f531c85fc440cfceadb96
Your Name <your.email@example.com>
2026-02-09 11:08:38 +0300

    added market link

--- farmart-frontend/src/components/Navbar.jsx
+++ farmart-frontend/src/components/Navbar.jsx
@@ -1,12 +1,2 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+<Link to="/">Home</Link>
 
-export default function Navbar() {
-  return (
-    <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/test">Test</Link>
-      <Link to="/cart">Cart</Link>
-    </nav>
-  )
-}
