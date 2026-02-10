df889ed179b283d4f4b9bc723e38e72c1df9b2f4
Your Name <your.email@example.com>
2026-02-10 15:14:35 +0300

    Created variable in  UserSlice

--- farmart-frontend/src/features/users/userSlice.js
+++ farmart-frontend/src/features/users/userSlice.js
@@ -2,3 +2,16 @@ import { createSlice } from '@reduxjs/toolkit'
 
 
 const initialState = { user: null, status: 'idle' }
+
+const userSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      state.user = action.payload
+    },
+    clearUser(state) {
+      state.user = null
+    },
+  },
+})
