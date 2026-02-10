3494d8f0bde7be40d5cd259b23b4541da77ad18b
Your Name <your.email@example.com>
2026-02-10 15:09:55 +0300

    Created the animal variable const

--- farmart-frontend/src/features/cart/cartSlice.js
+++ farmart-frontend/src/features/cart/cartSlice.js
@@ -1,1 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit'
+
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      state.items.push(action.payload)
+    },
+    removeFromCart(state, action) {
+      state.items = state.items.filter(i => i.id !== action.payload)
+    },
+  },
+})
