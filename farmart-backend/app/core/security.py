966bafe368fe86d20716fecd7c37c5b8952f5eff
Raphael Njoroge <raphael.njoroge@student.moringaschool.com>
2026-02-12 11:53:56 +0300

    Add security

---
+++ farmart-backend/app/core/security.py
@@ -0,0 +1,20 @@
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+import os
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(password, hashed):
+    return pwd_context.verify(password, hashed)
+
+def create_access_token(data: dict, expires_minutes=60):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=expires_minutes)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
