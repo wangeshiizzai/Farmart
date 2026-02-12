492c3a5a5d318d3cbae8e84edfc7a0c9557faf14
Raphael Njoroge <raphael.njoroge@student.moringaschool.com>
2026-02-12 11:49:16 +0300

    Add main

--- farmart-backend/app/main.py
+++ farmart-backend/app/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
-from app.api.routes import auth, animals, orders, payments
+from app.routes import auth, animals, orders, cart, payments
+from app.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Farmart API")
 
-app.include_router(auth.router, prefix="/auth", tags=["Auth"])
-app.include_router(animals.router, prefix="/animals", tags=["Animals"])
-app.include_router(orders.router, prefix="/orders", tags=["Orders"])
-app.include_router(payments.router, prefix="/payments", tags=["Payments"])
-
+app.include_router(auth.router)
+app.include_router(animals.router)
+app.include_router(orders.router)
+app.include_router(cart.router)
+app.include_router(payments.router)
 
 @app.get("/")
 def root():
-    return {"message": "Farmart backend running"}
+    return {"status": "Farmart backend running"}
