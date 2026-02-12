b54e5f295002161c5b2bced2a4b1e52b76062e4b
Raphael Njoroge <raphael.njoroge@student.moringaschool.com>
2026-02-12 16:32:37 +0300

    Add auth

---
+++ farmart-backend/app/routes/auth.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.models.user import User
+from app.schemas.user import UserCreate, UserLogin
+from app.core.security import hash_password, verify_password, create_access_token
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+@router.post("/register")
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    exists = db.query(User).filter(User.email == user.email).first()
+    if exists:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = User(
+        email=user.email,
+        password=hash_password(user.password),
+        role=user.role
+    )
+
+    db.add(new_user)
+    db.commit()
+    return {"message": "User registered successfully"}
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user or not verify_password(user.password, db_user.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    token = create_access_token({
+        "user_id": str(db_user.id),
+        "role": db_user.role
+    })
+
+    return {"access_token": token, "token_type": "bearer"}
