efac602564b5623b05224255a46ab31e0f840cb4
Raphael Njoroge <raphael.njoroge@student.moringaschool.com>
2026-02-12 16:29:13 +0300

    Add user

---
+++ farmart-backend/app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Enum
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(Enum("FARMER", "USER", name="user_roles"), nullable=False)
